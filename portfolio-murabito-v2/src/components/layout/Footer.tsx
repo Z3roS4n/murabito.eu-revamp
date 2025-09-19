6060592b1de805f8739bbf2bcab723c131853d46
Antonio Murabito <Thepro007002001@gmail.com>
2025-09-19 16:55:01 +0200

    Add and update Footer component in homepage
    Imported and rendered the Footer component in the main page. Updated Footer to include P.IVA information and improved layout with centered text.

--- portfolio-murabito-v2/src/components/layout/Footer.tsx
+++ portfolio-murabito-v2/src/components/layout/Footer.tsx
@@ -2,8 +2,9 @@ import { Heart } from "lucide-react";
 
 const Footer = () => {
   return (
-    <div className="bg-black relative overflow-hidden p-20">
-        <div className="flex flex-col w-1/1 justify-center">
+    <div className="relative overflow-hidden p-20">
+        <div className="flex flex-col w-1/1 justify-center text-center">
+            <p>P.IVA: 07344720821</p>
             <p className="flex flex-row gap-2">Built with <Heart/> by Antonio Murabito</p>
         </div>
     </div>
